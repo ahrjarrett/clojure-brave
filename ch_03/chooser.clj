afeaad51c405770942024847d96405207ebdfa24
Andrew Jarrett <ahrjarrett@gmail.com>
2017-03-21 00:53:27 -0600

    practicing destructuring with chooser.clj

---
+++ ch_03/chooser.clj
@@ -0,0 +1,10 @@
+(defn chooser
+  [[first-choice second-choice & unimportant-choices]]
+  (println (str "Your first choice is: " first-choice))
+  (println (str "Your second choice is: " second-choice))
+  (println (str "We’re ignoring the rest of your choices. "
+                "Here they are in case you need to cry over them: "
+                (clojure.string/join ", " unimportant-choices))))
+
+; don’t forget to put the params in a vector in this case:
+(chooser ["Gymnastics" "Olivetti Lettera 32" "beach balls"])
