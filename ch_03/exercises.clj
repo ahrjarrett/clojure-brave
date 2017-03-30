bf162c5eef349c1b33561864ad41edcecb08b537
Andrew Jarrett <ahrjarrett@gmail.com>
2017-03-29 18:08:24 -0600

    added chapter 3 exercises list

---
+++ ch_03/exercises.clj
@@ -0,0 +1,28 @@
+; 1: Use the str, vector, list, hash-map & hash-set fns
+
+
+; 2: Write a function that takes a number and adds 100 to it
+
+
+; 3: Write a function, dec-maker, that works exactly like
+;    the function inc-maker except with subtraction, like so:
+;    (def dec9 (dec-maker 9))
+;    (dec9 10)) ; => 1
+
+
+; 4: Write a function, mapset, that works like map,
+;    except the return value is a set
+
+
+; 5: Create a function that’s similar to symmetrize-body-parts
+;    except that it has to work with weird space aliens with
+;    radial symmetry. Instead of two eyes, arms, legs, and so on,
+;    they have five
+
+
+; 6: Create a function that generalizes symmetrize-body-parts
+;    and the func- tion you created in Exercise 5. The new function
+;    should take a col- lection of body parts and the number of
+;    matching body parts to add.
+
+
