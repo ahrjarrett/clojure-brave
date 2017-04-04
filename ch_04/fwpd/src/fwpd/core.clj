f42b63161a4f520d1f1b3cdb6eb5bcb69006cb3f
Andrew Jarrett <ahrjarrett@gmail.com>
2017-04-04 09:15:33 -0600

    page 95, (parse (slurp filename)) has wrong arity

--- ch_04/fwpd/src/fwpd/core.clj
+++ ch_04/fwpd/src/fwpd/core.clj
@@ -1,7 +1,26 @@
-(ns fwpd.core
-  (:gen-class))
+(ns fwpd.core)
+(def filename "suspects.csv")
+(def vamp-keys [:name :glitter-index])
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn str->int
+  [str]
+  (Integer. str))
+
+(def conversions {:name identity :glitter-index str->int})
+
+(defn convert
+  [vamp-key value]
+  ((get conversions vamp-key) value))
+  ; (convert :glitter-index "5") ; => 5
+
+(defn my-parse
+  "Convert a CSV into rows of columns"
+  [string]
+  (map (#(clojure.string/split % #","))
+       (clojure.string/split string #"\n")))
+
+; BOOKMARK: Page 95, can't get (parse (slurp filename)) to work
+
+
+
+
