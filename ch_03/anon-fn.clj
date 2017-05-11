232cbdd232d53c8c5f819f29e46c355032c818e5
Andrew Jarrett <ahrjarrett@gmail.com>
2017-05-11 01:13:48 -0600

    3: hobbits file core2.clj added, runs in CIDER

--- ch_03/anon-fn.clj
+++ ch_03/anon-fn.clj
@@ -4,22 +4,32 @@
 (def my-special-multiplier (fn [n] (* n 3)))
 (my-special-multiplier 8) ; 24
 
-; Shorthand
-; Where #() signifies an anon fn, and % is the param
+;; Shorthand
+;; Where #() signifies an anon fn, and % is the param
 (#(* % 3) 8) ; 24
 
-; Fn at top, rewritten:
+;; Fn at top, rewritten:
 (map #("Hi, " %)
   ["Curt Cobain" "Tobias Funke"])
 
-; NOTES:
-; Kinda weird not declaring the params in a vector,
-; instead jumping straight to using them in the return statement
+;; NOTES:
+;; Kinda weird not declaring the params in a vector,
+;; instead jumping straight to using them in the return statement
 
-; Anon fns with multiple parameters
-; % is equivalent to %1, counts up from there (%2, %3)
+;; Anon fns with multiple parameters
+;; % is equivalent to %1, counts up from there (%2, %3)
 (#(str %1 " and " %2) "beefy blocks" "whiskey lemonade")
 
-; Anon fns with rest paramenters
-; (Using the built-in identity function below)
+;; Anon fns with rest paramenters
+;; (Using the built-in identity function below)
 (#(identity %&) "Babycakes" "Steve" "Frank") ; ("Babycakes" "Steve" "Frank")
+(#(clojure.string/join %&) "Bisu" "Savior" "Boxer") ;; => "BisuSaviorBoxer"
+
+
+;; Functions that return functions:
+(defn inc-maker
+  [inc-by]
+  #(+ % inc-by))
+(def inc3
+  (inc-maker 3))
+(inc3 7)
---
+++ ch_03/hobbits/src/hobbits/core2.clj
@@ -0,0 +1,70 @@
+(ns hobbits.core2)
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 3}
+                             {:name "left-ear" :size 3}
+                             {:name "mouth" :size 3}
+                             {:name "nose" :size 3}
+                             {:name "neck" :size 3}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 3}
+                             {:name "back" :size 3}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 3}
+                             {:name "left-kidney" :size 3}
+                             {:name "left-hand" :size 3}
+                             {:name "left-knee" :size 3}
+                             {:name "left-thigh" :size 3}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 3}
+                             {:name "left-foot" :size 3}])
+
+(defn matching-part [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+;; What's cool is how well this reads:
+(defn symmetrize-body-parts [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts
+         final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+               (into final-body-parts
+                     (set [part (matching-part part)])))))))
+
+(symmetrize-body-parts asym-hobbit-body-parts)
+
+;; We can do better by using REDUCE:
+(defn better-symmetrize-body-parts [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts))
+
+(better-symmetrize-body-parts asym-hobbit-body-parts)
+
+
+;; TODO:
+;; What if we could write a more generic program
+;; that takes an expander function instead of a matching-part function?
+(defn expand-body-parts [expander model]
+  ;;...
+  ;;...
+)
+
+(defn hit [asym-body-parts]
+  (let [sym-parts (better-symmetrize-body-parts asym-body-parts)
+        body-part-size-sum (reduce + (map :size sym-parts))
+        target (rand body-part-size-sum)]
+    (loop [[part & remaining] sym-parts
+           accumulated-size (:size part)]
+      (if (> accumulated-size target)
+        part
+        (recur remaining (+ accumulated-size (:size (first remaining))))))))
+
+(hit asym-hobbit-body-parts)
+
+
