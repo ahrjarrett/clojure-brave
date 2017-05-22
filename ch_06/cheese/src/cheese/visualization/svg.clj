8cc306c2692bd90842e77cf5fb62881bb96fc5e1
Andrew Jarrett <ahrjarrett@gmail.com>
2017-05-21 22:43:44 -0600

    ch 06: namespaces

---
+++ ch_06/cheese/src/cheese/visualization/svg.clj
@@ -0,0 +1,22 @@
+(ns cheese.visualization.svg)
+
+(defn latlng->point
+  [latlng]
+  (str (:lng latlng) "," (:lat latlng)))
+
+(defn points
+  [locations]
+  (clojure.string/join " " (map latlng->point locations)))
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ch_06/namespaces.clj
@@ -0,0 +1,12 @@
+(ns-name *ns*) ;; => user
+
+(ns-map *ns*)
+
+;; You can also browse namespaces in Spacemacs CIDER using:
+;; , h n
+
+(def stuff "stuffffff")
+
+
+
+
