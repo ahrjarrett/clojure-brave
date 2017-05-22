04b8c5de31ed8df514876eb8bf0fff3620c293aa
Andrew Jarrett <ahrjarrett@gmail.com>
2017-05-21 23:37:20 -0600

    finished chapter 6, project works in the browser

--- ch_06/cheese/src/cheese/visualization/svg.clj
+++ ch_06/cheese/src/cheese/visualization/svg.clj
@@ -1,4 +1,16 @@
-(ns cheese.visualization.svg)
+(ns cheese.visualization.svg
+  (:require [clojure.string :as s])
+  (:refer-clojure :exclude [min max]))
+
+(defn comparator-over-maps
+  [comparison-fn ks]
+  (fn [maps]
+    (zipmap ks
+            (map (fn [k] (apply comparison-fn (map k maps)))
+                 ks))))
+
+(def min (comparator-over-maps clojure.core/min [:lat :lng]))
+(def max (comparator-over-maps clojure.core/max [:lat :lng]))
 
 (defn latlng->point
   [latlng]
@@ -7,6 +19,63 @@
 (defn points
   [locations]
   (clojure.string/join " " (map latlng->point locations)))
+
+;;;; p. 141:
+;;;; This isn't working for me? Getting a null pointer exception.
+;(min [{:a 1 :b 3} {:a 5 :b 0}])
+
+(zipmap [:a :b] ["elmo" "cookie monster"]) ;; => {:a "elmo", :b "cookie monster"}
+
+(defn translate-to-00
+  [locations]
+  (let [mincoords (min locations)]
+    (map #(merge-with - % mincoords) locations)))
+
+(defn scale
+  [width height locations]
+  (let [maxcoords (max locations)
+        ratio {:lat (/ height (:lat maxcoords))
+               :lng (/ width (:lng maxcoords))}]
+    (map #(merge-with * % ratio) locations)))
+
+(defn latlng->point
+  [latlng]
+  (str (:lng latlng) "," (:lat latlng)))
+
+(defn points
+  [locations]
+  (s/join " " (map latlng->point locations)))
+
+(defn line
+  [points]
+  (str "<polyline points=\"" points "\" />"))
+
+(defn transform
+  "Just chains other functions"
+  [width height locations]
+  (->> locations
+       translate-to-00
+       (scale width height)))
+
+(defn xml
+  "svg 'template', which also flips the coordinate system"
+  [width height locations]
+  (str "<svg height=\"" height "\" width=\"" width "\">"
+       ;; These two <g> tags flip the coordinate system
+       "<g transform=\"translate(0," height ")\">"
+       "<g transform=\"scale(1,-1)\">"
+       (-> (transform width height locations)
+           points
+           line)
+       "</g></g>"
+       "</svg>"))
+
+
+
+
+
+
+
 
 
 
